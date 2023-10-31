4be8cb2e8a258e3ea8737c7e1a94359f61cee408
Vinicius Roberto de Oliveira Santos <vroliveira.santos@gmail.com>
2023-10-31 10:39:58 -0300

    Adicionar migration Auth

--- ApiAuth/Domain/Entities/UsuarioEntity.cs
+++ ApiAuth/Domain/Entities/UsuarioEntity.cs
@@ -2,6 +2,7 @@
 {
     public class UsuarioEntity
     {
+        public int Id { get; private set; }
         public string NomeUsuario { get; set; }
         public string Senha { get; set; }
     }
---
+++ ApiAuth/Migrations/20231031133846_UsuarioAuth.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace _3BPACS.ApiAuth.Migrations
+{
+    /// <inheritdoc />
+    public partial class UsuarioAuth : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Usuarios",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NomeUsuario = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Senha = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuarios", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Usuarios");
+        }
+    }
+}
